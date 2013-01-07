c5290de415a96cfbf96de3bdbceef0e3dbd5793f
Vijay Kumbhar <kvijay@weboniselab.com>
2013-01-07 19:43:59 +0530

    working on the transloc api poc

---
+++ transloc/routes.php
@@ -0,0 +1,68 @@
+<?php
+include_once('curl.php');
+//echo "api.transloc.com/1.1/routes.json?agencies=".$_GET['agency_id']."";
+
+$agencyId = $_GET['agency_id'];
+
+$curl = new CURLCalls();
+
+$curlRoutesUrl = "api.transloc.com/1.1/routes.json?agencies=".$agencyId."";
+
+$routes = $curl->getResponse($curlRoutesUrl);
+$ttaRoutes = $routes->data->{$agencyId};
+
+//$curlStopsUrl = "api.transloc.com/1.1/routes.json?agencies=".$agencyId."";
+//
+//$stops = $curl->getResponse($curlStopsUrl);
+////
+//echo '<pre> object is======';
+//print_r($routes->data->{$agencyId});
+//echo '</pre>';
+/**
+ * show the list of agencies
+ */
+?>
+<table border="1">
+    <tr>
+         <td colspan="4">Routes & Stops for the TTA </td>
+    </tr>
+    <?php
+    if (isset($ttaRoutes) && !empty($ttaRoutes)) {
+
+        foreach ($ttaRoutes as $route) {
+            ?>
+            <tr>
+                <td>Route Id : <?php echo $route->route_id;?></td>
+                <td>Route Name : <?php echo $route->long_name;?></td>
+                <td>Route Short Name : <?php echo $route->short_name;?></td>
+                <td>Route Type : <?php echo $route->type;?></td>
+            </tr>
+
+            <tr>
+                <td colspan="4">&nbsp;</td>
+            </tr>
+
+            <tr>
+                <td colspan="4"><b>Stops</b></td>
+            </tr>
+
+            <tr>
+                <td colspan="4">&nbsp;</td>
+            </tr>
+                <?php
+            //if(){
+
+            //}
+            ?>
+
+            <?php
+        }
+    } else {
+        ?>
+        <tr>
+            <td colspan="4">No routes data to display</td>
+        </tr>
+        <?php
+    }
+    ?>
+</table>
